41789f328b0da501557d73988f97112768f4e05a
원더 <wonderbotsupport@naver.com>
2020-12-09 13:45:16 +0900

    feat: update

--- src/routes/Mac.tsx
+++ src/routes/Mac.tsx
@@ -16,7 +16,7 @@ export default function Mac () {
             <p style={{ fontSize: 25, fontWeight: 600 }}>English의 흐름을 바꾸다.</p>
             <p style={{ fontSize: 17, fontWeight: 400 }}>₩1,290,000부터</p>
             <p style={{ fontSize: 14, fontWeight: 400, color: '#a1a1a6' }}>출시일은 추후 공개됩니다.</p>
-            <p><Button color="primary" style={{ color: '#fff', background: '#2997ff !Important', fontSize: '15px', fontWeight: 400, borderRadius: 22, padding: '10px 20px' }}>가격 보기</Button></p>
+            <p><Button color="primary" style={{ color: '#fff', background: '#2997ff !Important', fontSize: '15px', fontWeight: 400, borderRadius: 22, padding: '10px 20px' }}>구입하기</Button></p>
           </Col>
         </Row>
 
@@ -29,7 +29,6 @@ export default function Mac () {
             <h2 style={{ fontWeight: 600, fontSize: 56 }}><span style={{ fontSize: 21, fontWeight: 700 }}>13형 모델</span><br/>MacBook Math</h2>
             <p style={{ fontSize: 25, fontWeight: 600 }}>Math중에 수학.</p>
             <p style={{ fontSize: 17, fontWeight: 400 }}>₩1,690,000부터</p>
-            <p style={{ fontSize: 14, fontWeight: 400, color: '#a1a1a6' }}>M1 칩 탑재 모델의 출시일은<br/>추후 공개됩니다.</p>
             <p><Button color="primary" style={{ color: '#fff', background: '#2997ff !Important', fontSize: '15px', fontWeight: 400, borderRadius: 22, padding: '10px 20px' }}>구입하기</Button></p>
           </Col>
         </Row>
@@ -37,10 +36,9 @@ export default function Mac () {
         <Row style={{ paddingBottom: '80px', backgroundImage: 'url("https://www.apple.com/v/mac/home/aw/images/overview/hero/dark/mac_mini__gc148mlvnziq_large_2x.jpg")', backgroundRepeat: 'no-repeat', backgroundPosition: '500px 51px', backgroundSize: '666px 422px' }}>
           <Col xs="6" >
             <span style={{ color: '#f56300', textAlign: 'left' }}>New</span>
-            <h2 style={{ fontWeight: 600, fontSize: 56 }}>MacBook Science</h2>
+            <h2 style={{ fontWeight: 600, fontSize: 56 }}>Mac Science</h2>
             <p style={{ fontSize: 25, fontWeight: 600 }}>새로운 원소. 어마무시한 가능성.</p>
             <p style={{ fontSize: 17, fontWeight: 400 }}>₩890,000부터</p>
-            <p style={{ fontSize: 14, fontWeight: 400, color: '#a1a1a6' }}>M1 칩 탑재 모델의 출시일은<br/>추후 공개됩니다.</p>
             <p><Button color="primary" style={{ color: '#fff', background: '#2997ff !Important', fontSize: '14px', fontWeight: 400, borderRadius: 22, padding: '10px 20px' }}>구입하기</Button></p>
           </Col>
           <Col xs="6">
