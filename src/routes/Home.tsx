dee688eb46df475ce920464ed9d35d57a4739be0
원더 <wonderbotsupport@naver.com>
2020-12-10 09:30:31 +0900

    fix: video

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -4,7 +4,7 @@ import { Button, Container } from 'reactstrap'
 export default function Home () {
   return (
       <div style={{ position: 'absolute', width: '100%', height: '100%' }}>
-        <video autoPlay muted loop style={{ position: 'absolute', minWidth: '100%', minHeight: '100%', objectFit: 'cover', width: 'auto', height: 'auto' }} id="airpodsMax" src="https://www.apple.com/105/media/us/homepage/2020/ee22f441-0fc6-4294-b7b0-1525874a8f8b/anim/hero/large.mp4" playsInline>
+        <video autoPlay muted loop style={{ position: 'absolute', minWidth: '100%', minHeight: '100%', objectFit: 'cover', width: 'auto', height: 'auto' }} id="airpodsMax" src="/airpods-max.mp4" playsInline>
         </video>
         <div style={{ textAlign: 'center', position: 'absolute', width: '100%', left: 0, color: "white", paddingTop: '30vh' }}>
           <h1 style={{ fontSize: '7em', fontWeight: 600 }}>Airpods Max</h1>
